9da5f087c823b01030e8484c264b731f392d6922
Samuel Zandi <samuelczandi@gmail.com>
2020-01-02 23:48:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,11 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  counter = 0 
+  while counter < collection.length 
+  
+  counter += 1
+end
 end
 
 def consolidate_cart(cart)
