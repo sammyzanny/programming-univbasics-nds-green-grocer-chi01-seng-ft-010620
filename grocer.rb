860726194151fc41845b4efed1d0f62b25ce3f07
Samuel Zandi <samuelczandi@gmail.com>
2020-01-03 00:25:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,8 +4,8 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   counter = 0 
   while counter < collection.length 
-  if collection[counter]
-    
+  if collection[counter][:item] == name
+    return collection[counter]
   end
   counter += 1
 end
