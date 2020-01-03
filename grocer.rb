aa53900a31fbd1e4edec82cadbc782bf40249b60
Samuel Zandi <samuelczandi@gmail.com>
2020-01-03 01:03:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -90,5 +90,6 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  
+  consilidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons()
 end
