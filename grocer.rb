d6a95f9afaf6d4de9e14403e04a9d29b3735f3d5
Samuel Zandi <samuelczandi@gmail.com>
2020-01-03 00:44:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,9 @@ def apply_coupons(cart, coupons)
   counter = 0 
   while counter < coupons.length do 
    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-   couponed_item_name = 
+   couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+   cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+   if cart_item 
    counter += 1 
   end  
 end
